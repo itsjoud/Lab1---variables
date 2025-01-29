b172887c57173ebc1dce5656ae0fd009b4040a67
Joud Herzallah <89647291+itsjoud@users.noreply.github.com>
2025-01-29 19:01:21 +0400

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,70 @@
+// //question 1
+// fn main() {
+//         // TODO: Add the missing keyword.
+//        let x = 5;
+//         println!("x has the value {x}");
+
+// }
+
+
+
+ // ----------------------------------------------------
+//question 2
+// fn main() {
+//     // TODO: Change the line below to fix the compiler error.
+//     let x: u8=5;
+
+//     if x == 10 {
+//         println!("x is ten!");
+//     } else {
+//         println!("x is not ten!");
+//     }
+// }
+
+
+
+ // ----------------------------------------------------
+////question 3
+// fn main() {
+//     // TODO: Change the line below to fix the compiler error.
+//     let x: i32 =15;
+//     println!("Number {x}");
+// }
+
+
+ // ----------------------------------------------------
+
+// //question 4
+
+// // TODO: Fix the compiler error.
+// fn main() {
+//     let mut x = 3;
+//     println!("Number {x}");
+
+//     x = 5; // Don't change this line
+//     println!("Number {x}");
+// }
+
+
+// ----------------------------------------------------
+
+// // //question 5
+// fn main() {
+//     let number = "T-H-R-E-E"; // Don't change this line
+//     println!("Spell a number: {}", number);
+
+//     // TODO: Fix the compiler error by changing the line below without renaming the variable.
+//     let number : i32 = 3;
+//     println!("Number plus two is: {}", number + 2);
+// }
+
+
+// ----------------------------------------------------
+
+// // //question 6
+// // TODO: Change the line below to fix the compiler error.
+// const NUMBER :i32 = 3;
+
+// fn main() {
+//     println!("Number: {NUMBER}");
+// }
